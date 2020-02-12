61aaf8b48b5f47442338ea4ced10cbeed3d48c44
Johnson Kow <jkow95@gmail.com>
2020-02-12 19:51:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -2,7 +2,7 @@
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
@@ -12,7 +12,7 @@ def turtle_traits(turtles)
   new = []
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
-      new << trait
+      trait
     end
   end
 end
